0f103277b1c971e89d171109ec190a4e012d5cd1
Theresia <e1375477@u.nus.edu>
2025-06-11 19:52:17 +0700

    theresia/debug image path

--- app/loginScreen/popup.tsx
+++ app/loginScreen/popup.tsx
@@ -1,19 +1,31 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function PaymentPopup({ onPay, onCancel }: { onPay: () => void; onCancel: () => void }) {
+export default function PaymentPopup({
+  onPay,
+  onCancel,
+}: {
+  onPay: () => void;
+  onCancel: () => void;
+}) {
   return (
     <View style={styles.popupContainer}>
       <Text style={styles.title}>Weekly Tutoring Session</Text>
       <View style={styles.separator} />
 
       <View style={styles.row}>
-        <Image source={require("../assets/images/hatLogo.png")} style={styles.avatar} />
+        <Image
+          source={require("../../assets/images/hatLogo.png")}
+          style={styles.avatar}
+        />
         <Text style={styles.label}>Paid To</Text>
         <Text style={styles.name}>Full Name</Text>
       </View>
 
       <View style={styles.row}>
-        <Image source={require("../assets/images/hatLogo.png")} style={styles.avatar} />
+        <Image
+          source={require("../../assets/images/hatLogo.png")}
+          style={styles.avatar}
+        />
         <Text style={styles.label}>Paid By</Text>
         <Text style={styles.name}>Full Name</Text>
       </View>
@@ -23,9 +35,12 @@ export default function PaymentPopup({ onPay, onCancel }: { onPay: () => void; o
       <Text style={styles.sectionLabel}>Details</Text>
 
       <View style={styles.detailRow}>
-        <Image source={require("../assets/images/calender.png")} style={styles.avatar}/>
-          <Text style={styles.detailText}>Tuesday [6pm - 8pm] - MATH</Text>
-          <Text style={styles.detailText}>Friday [6pm - 8pm] - PHYSICS</Text>
+        <Image
+          source={require("../assets/images/calender.png")}
+          style={styles.avatar}
+        />
+        <Text style={styles.detailText}>Tuesday [6pm - 8pm] - MATH</Text>
+        <Text style={styles.detailText}>Friday [6pm - 8pm] - PHYSICS</Text>
       </View>
 
       <View style={styles.detailRow}>
@@ -52,56 +67,56 @@ export default function PaymentPopup({ onPay, onCancel }: { onPay: () => void; o
 
 const styles = StyleSheet.create({
   popupContainer: {
-    backgroundColor: '#FEEFCB',
+    backgroundColor: "#FEEFCB",
     padding: 20,
     borderRadius: 20,
     margin: 20,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.2,
     shadowRadius: 10,
     elevation: 5,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#803300',
-    textAlign: 'center',
+    fontWeight: "bold",
+    color: "#803300",
+    textAlign: "center",
     marginBottom: 10,
   },
   separator: {
     borderBottomWidth: 2,
-    borderColor: '#FFD247',
+    borderColor: "#FFD247",
     marginVertical: 10,
   },
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 10,
   },
   avatar: {
     width: 40,
     height: 40,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginRight: 10,
   },
   label: {
-    fontWeight: 'bold',
-    color: '#803300',
+    fontWeight: "bold",
+    color: "#803300",
     marginRight: 10,
   },
   name: {
-    color: '#803300',
+    color: "#803300",
     fontSize: 16,
   },
   sectionLabel: {
-    fontWeight: 'bold',
-    color: '#803300',
+    fontWeight: "bold",
+    color: "#803300",
     fontSize: 16,
     marginBottom: 5,
   },
   detailRow: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    flexDirection: "row",
+    alignItems: "flex-start",
     marginBottom: 8,
   },
   icon: {
@@ -110,47 +125,46 @@ const styles = StyleSheet.create({
   },
   detailText: {
     fontSize: 15,
-    color: '#803300',
+    color: "#803300",
   },
   totalRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginVertical: 10,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   yellowLine: {
     flex: 1,
     height: 2,
-    backgroundColor: '#FFD247',
+    backgroundColor: "#FFD247",
   },
   totalText: {
     marginHorizontal: 8,
-    fontWeight: 'bold',
-    color: '#803300',
+    fontWeight: "bold",
+    color: "#803300",
   },
   payButton: {
-    backgroundColor: '#28C740',
+    backgroundColor: "#28C740",
     paddingVertical: 12,
     borderRadius: 12,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
   },
   payButtonText: {
     fontSize: 18,
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
   },
   cancelButton: {
-    backgroundColor: '#F07C5B',
+    backgroundColor: "#F07C5B",
     paddingVertical: 12,
     borderRadius: 12,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
   },
   cancelButtonText: {
     fontSize: 18,
-    color: '#803300',
-    fontWeight: 'bold',
+    color: "#803300",
+    fontWeight: "bold",
   },
 });
-
